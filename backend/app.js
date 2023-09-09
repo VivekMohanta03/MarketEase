e0332bb0988c180cdab0e33203ace4fe489cae8a
Vivek Kumar Mohanta <vivek123.mohanta@gmail.com>
2023-09-09 20:13:01 +0530

    Changes and Bug fixes

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 app.use(cors({
   origin: ['http://localhost:3000',],
   credentials: true
