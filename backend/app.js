8d9394b5b9e11bc2ef18d70396333f0710c7eb06
Vivek Kumar Mohanta <vivek123.mohanta@gmail.com>
2023-09-09 22:08:51 +0530

    backend bug fixes

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
